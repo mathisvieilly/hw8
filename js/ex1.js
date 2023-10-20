d94b3cd47a48eae925d039fad64501bd736fb952
Mathis VIEILLY <mathis.vieilly@epitech.eu>
2023-10-19 20:54:08 -0700

    first commit

---
+++ js/ex1.js
@@ -0,0 +1,25 @@
+fetch('https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/paintings.json')
+  .then(response => response.json())
+  .then(paintings => {
+    console.log(paintings);
+    const paintingList = document.getElementById('paintings');
+
+    paintings.forEach(painting => {
+      const tr = document.createElement('tr');
+      const td1 = document.createElement('td');
+      const td2 = document.createElement('td');
+      const td3 = document.createElement('td');
+
+      td1.textContent = painting.name;
+      td2.textContent = painting.year;
+      td3.textContent = painting.artist;
+
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+
+      paintingList.appendChild(tr);
+    });
+  }).catch(err => {
+    console.error(err);
+  });
---
+++ js/ex2.js
@@ -0,0 +1,26 @@
+const informationTable = document.getElementById("information");
+informationTable.style.display = 'none';
+
+document.getElementById("lookup").addEventListener("click", event => {
+  const value = document.getElementById("login").value;
+  if (value === "") {
+    return;
+  }
+
+  fetch("https://api.github.com/users/" + value)
+  .then(response => response.json())
+  .then(data => {
+    if (data.message) {
+      alert(data.message);
+      return;
+    }
+
+    document.getElementById("avatar").src = data.avatar_url;
+    document.getElementById("name").innerText = data.name;
+    document.getElementById("blog").innerText = data.blog;
+    document.getElementById("created").innerText = data.created_at;
+    informationTable.style.display = '';
+  }).catch(err => {
+    console.error(err.message);
+  });
+});
